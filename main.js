578bcc6ecb8bcdf5ed2dc73d7f294e8ab13fbca8
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2023-12-07 14:40:34 +0100

    import earth model and moon turn around

--- main.js
+++ main.js
@@ -1,22 +1,66 @@
 import * as THREE from 'three';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // Position the camera
-camera.position.z = 5;
+camera.position.z = 3;
 
-// Create a black sphere
-const geometry = new THREE.SphereGeometry(1, 32, 32);
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const sphere = new THREE.Mesh(geometry, material);
-// Add the sphere to the scene
-scene.add(sphere);
+let earth;
+// load GLTF model
+const loader = new GLTFLoader();
+loader.load('./earth.glb', function (gltf) {
+    // Scale down the model
+    const scale = 0.0013; // Adjust this value as needed
+    earth = gltf.scene;
+    gltf.scene.scale.set(scale, scale, scale);
 
-// add lightning around the sphere
-const light = new THREE.PointLight(0xffffff, 100, 1000);
-light.position.set(0, 0, 0);
-scene.add(light);
+    // Calculate the new bounding box
+    const bbox = new THREE.Box3().setFromObject(gltf.scene);
+    const center = bbox.getCenter(new THREE.Vector3());
+
+    // Center the model
+    gltf.scene.position.x = -center.x;
+    gltf.scene.position.y = -center.y;
+    gltf.scene.position.z = -center.z;
+
+    gltf.scene.rotation.y = Math.PI;
+    scene.add(gltf.scene);
+
+    // Add a soft white ambient light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 5);
+    scene.add(ambientLight);
+
+    // console log bbox
+    console.log(bbox);
+
+    gltf.scene.traverse(function (child) {
+        if (child.isMesh) {
+            child.material = new THREE.MeshStandardMaterial({
+                color : child.material.color,
+                map: child.material.map,
+            })
+        }
+    })
+
+    
+    console.log('Model loaded:', gltf);
+
+}, undefined, function (error) {
+    console.error(error);
+});
+
+const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(0.1, 32, 32),
+    new THREE.MeshStandardMaterial({
+        color: 0xaaaaaa,
+    })
+);
+
+moon.position.set(1, 0, 0);
+
+scene.add(moon);
 
 // if I wheel the mouse, the camera will zoom in and out
 document.addEventListener('wheel', (event) => {
@@ -24,8 +68,8 @@ document.addEventListener('wheel', (event) => {
 });
 
 
-for (let i = 0; i < 1000; i++) {
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
+for (let i = 0; i < 3000; i++) {
+    const geometry = new THREE.SphereGeometry(0.33, 8, 8);
     const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
     const sphere = new THREE.Mesh(geometry, material);
     sphere.position.x = Math.random() * 1000 - 500;
@@ -37,18 +81,21 @@ for (let i = 0; i < 1000; i++) {
 // Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000, 1);
-document.body.appendChild(renderer.domElement);
+renderer.setClearColor(0x000, 1);
+document.getElementById("header").appendChild(renderer.domElement);
 
-// Render loop
+let angle = 0;
+let orbitSpeed = 0.01;
+// Animate
 function animate() {
     requestAnimationFrame(animate);
-
-    // Rotate the sphere for a bit of animation
-    sphere.rotation.x += 0.01;
-    sphere.rotation.y += 0.01;
-
     renderer.render(scene, camera);
+    if (earth) {
+        earth.rotation.y += 0.01;
+    }
+    angle += orbitSpeed;
+    moon.position.x = Math.cos(angle) * 1;
+    moon.position.z = Math.sin(angle) * 1;
 }
 
-animate(); // Start the animation loop
+animate();
